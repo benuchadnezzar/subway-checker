bdfd7d01ebb03c4b8d36eff2e6cee4c5da0cf950
Ben Kulakofsky <benjamin.kulakofsky@gmail.com>
2017-10-17 10:53:14 -0400

    Changed URL in RequestMock

--- src/mocks/RequestMock.js
+++ src/mocks/RequestMock.js
@@ -3,13 +3,13 @@ var request = require('request');
 
 var requestSettings = {
   method: 'GET',
-  url: 'http://datamine.mta.info/mta_esi.php?key=YOUR_KEY_HERE&feed_id=2',
+  url: './MockPBufData',
   encoding: null
 };
 
 function RequestMock () {
 	request(requestSettings, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
+	  if (!error) {
 	  var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
 	  feed.entity.forEach(function(entity) {
 	  	if (entity.trip_update) {
